a132a1f9e6145f60d7a59cb7946b80aa79ef0c2b
Paulo Varassin <pauloarthurvm@hotmail.com>
2020-12-01 17:42:33 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/paulovarassin/springmongodb/resources/UserResource.java
+++ src/main/java/com/paulovarassin/springmongodb/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.paulovarassin.springmongodb.domain.Post;
 import com.paulovarassin.springmongodb.domain.User;
 import com.paulovarassin.springmongodb.dto.UserDTO;
 import com.paulovarassin.springmongodb.services.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
